a5e090dbabcdad1eab4348e7fc77d58affa349fd
dev <home@Pranavs-Mac-mini.local>
2024-05-22 03:59:54 +0530

    changed chart format

--- src/CH.js
+++ src/CH.js
@@ -6,38 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CH({token}) {
 
-    const [searchTerm, setSearchTerm] = useState('');
+    const [parameter,setParameter]=useState('VOLUME')
 
-    const [TV_DATA_ROUTE,setTV_DATA_ROUTE] = useState([
-        ["CEX", "24h Trading Volume", { role: "style" }],
-        ["Kucoin", 8.94, "#b87333"], // RGB value
-        ["MEXC", 10.49, "silver"], // English color name,
-        ["GATE", 21.45, "color: #e5e4e2"], // CSS-style declaration
-        ["ASCENDEX", 21.45, "green"],
-      ]);
   
-      const [SPREAD_DATA_ROUTE,setSPREAD_DATA_ROUTE] = useState([
-        ["CEX", "24h Trading Volume", { role: "style" }],
-        ["Kucoin", 8.94, "#b87333"], // RGB value
-        ["MEXC", 10.49, "silver"], // English color name
-        ["GATE", 21.45, "color: #e5e4e2"], // CSS-style declaration
-        ["ASCENDEX", 21.45, "green"],
-      ]);
-      const [DEPTH_DATA_ROUTE,setDEPTH_DATA_ROUTE] = useState([
-        [
-          "Depth",
-          "Kucoin",
-          "MEXC",
-          "GATE",
-          "ASENDEX"
-        ],
-        [0.3, 37.8, 80.8, 0,0],
-        [0.5, 30.9, 69.5,0,0],
-        [1, 25.4, 57, 25.7,0],
-        
-      ]);
-
-
   
     const options = {
           chartArea: {
@@ -49,77 +20,281 @@ function CH({token}) {
     
   };
 
-  const handleSearch = () => {
-   
-    const filtered = data.filter(item => item.time.toLowerCase().includes(searchTerm.toLowerCase()));
-    setData(filtered);
-  };
+ 
   
 
-    const [data,setData]=useState([{time:"May 19 2024",  volume : [136291.65367596,44565.84,48136,52061.042920694], spread:[ 0.00410000000000022,0.036,0.04664,0.00700000000000012], depth:[ {
-        "1%": 0,
-        "0.3%": 0,
-        "0.5%": 0
-      },{
-        "1%": 11.27536,
-        "0.5%": 0,
-        "0.3%": 0
-      },{
-        "1%": 0,
-        "0.3%": 0,
-        "0.5%": 0
-      },
-{
-        "1%": 11.27536,
-        "0.5%": 0,
-        "0.3%": 0
-      },{
-        "0.5%": 6439.71368,
-        "1%": 9648.5765,
-        "0.3%": 269.8122
-      }]}, {time:"May 19 2024",  volume : [136291.65367596,44565.84,48136,52061.042920694], spread:[ 0.00410000000000022,0.036,0.04664,0.00700000000000012], depth:[ {
-        "1%": 0,
-        "0.3%": 0,
-        "0.5%": 0
-      },{
-        "1%": 11.27536,
-        "0.5%": 0,
-        "0.3%": 0
-      },{
-        "1%": 0,
-        "0.3%": 0,
-        "0.5%": 0
-      },
-{
-        "1%": 11.27536,
-        "0.5%": 0,
-        "0.3%": 0
-      },{
-        "0.5%": 6439.71368,
-        "1%": 9648.5765,
-        "0.3%": 269.8122
-      }]}])
-
-    useEffect(()=>{
-
-        const fetchData=async ()=>{
-            const dataFromFireBase = await axios('https://proxy-j1k6.onrender.com/read')
-            const dataFromFireBase1=dataFromFireBase.data.map(item => ({
-                time: item.time,
-                volume: item.exchange.map(ex => parseFloat(ex.volume)),
-                spread: item.exchange.map(ex => ex.spread),
-                depth: item.exchange.map(ex => ex.depth)
-                }))
-            setData(dataFromFireBase1)
-
-
-        
+    const [data,setData]=useState([{
+      "exchange": [
+        {
+          "spread": 0.000300000000000189,
+          "volume": "199661.878488",
+          "depth": {
+            "0.5%": 573.24091316,
+            "0.3%": 305.56399956,
+            "1%": 1091.0106681
+          },
+          "name": "Kucoin"
+        },
+        {
+          "volume": "15760.88",
+          "spread": 0.0310000000000001,
+          "depth": {
+            "0.3%": 10.13816,
+            "0.5%": 418.0636,
+            "1%": 1240.79378
+          },
+          "name": "Mexc"
+        },
+        {
+          "spread": 0.0468600000000001,
+          "depth": {
+            "0.3%": 11.3446,
+            "0.5%": 345.41345,
+            "1%": 345.41345
+          },
+          "volume": "47939",
+          "name": "Ascendex"
+        },
+        {
+          "name": "Gate",
+          "spread": 0.00499999999999989,
+          "volume": "63855.151258841",
+          "depth": {
+            "0.5%": 4114.44393,
+            "1%": 11460.21868,
+            "0.3%": 564.12582
+          }
         }
-        
+      ],
+      "time": "May 19 2024",
+      "id": "0FJCJkGvysv89INTupQh"
+    }])
 
-        fetchData()
+      const [TV_DATA_KUCOIN,setTV_DATA_KUCOIN] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [TV_DATA_MEXC,setTV_DATA_MEXC] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [TV_DATA_ASD,setTV_DATA_ASD] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [TV_DATA_GATE,setTV_DATA_GATE] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+
+      const [SPREAD_DATA_KUCOIN,setSPREAD_DATA_KUCOIN] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [SPREAD_DATA_MEXC,setSPREAD_DATA_MEXC] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [SPREAD_DATA_ASD,setSPREAD_DATA_ASD] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+      const [SPREAD_DATA_GATE,setSPREAD_DATA_GATE] = useState([
+        ["CEX", "24h Trading Volume", { role: "style" }],
+        ["19 May 2024", 8.94, "#b87333"], // RGB value
+        ["19 May 2024", 10.49, "silver"], // English color name,
+        ["19 May 2024", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ["19 May 2024", 21.45, "green"],
+      ]);
+
+      const [DEPTH_DATA_KUCOIN,setDEPTH_DATA_KUCOIN] =useState([[
+        "Day",
+        "19 May 2024",
+        "20 May 2024",
+        "21 May 2024",
+      ],
+      [0.3, 305.56399956,  337.82230711, 365.82230711],
+      [0.5, 573.24091316,4054.76031, 3054.76031],
+      [1, 25.4, 57, 25.7]])
+
+      const [DEPTH_DATA_MEXC,setDEPTH_DATA_MEXC] =useState([[
+        "Day",
+        "19 May 2024",
+        "20 May 2024",
+        "21 May 2024",
+      ],
+      [0.3, 305.56399956,  337.82230711, 365.82230711],
+      [0.5, 573.24091316,4054.76031, 3054.76031],
+      [1, 25.4, 57, 25.7]])
+
+      const [DEPTH_DATA_ASD,setDEPTH_DATA_ASD] =useState([[
+        "Day",
+        "19 May 2024",
+        "20 May 2024",
+        "21 May 2024",
+      ],
+      [0.3, 305.56399956,  337.82230711, 365.82230711],
+      [0.5, 573.24091316,4054.76031, 3054.76031],
+      [1, 25.4, 57, 25.7]])
+
+      const [DEPTH_DATA_GATE,setDEPTH_DATA_GATE] =useState([[
+        "Day",
+        "19 May 2024",
+        "20 May 2024",
+        "21 May 2024",
+      ],
+      [0.3, 305.56399956,  337.82230711, 365.82230711],
+      [0.5, 573.24091316,4054.76031, 3054.76031],
+      [1, 25.4, 57, 25.7]])
+
+ 
+
+
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const dataFromFireBase = await axios('https://proxy-j1k6.onrender.com/read');
+        const dataFromFireBase1 = dataFromFireBase.data.map(item => ({
+          time: item.time,
+          volume: item.exchange.map(ex => parseFloat(ex.volume)),
+          spread: item.exchange.map(ex => ex.spread),
+          depth: item.exchange.map(ex => ex.depth)
+        }));
+        setData(dataFromFireBase1);
+    
+        const getRandomColor = () => {
+          const letters = '0123456789ABCDEF';
+          let color = '#';
+          for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+        };
+        
+        const createVolumeDataArray = (exchangeName) => {
+          const header = ["CEX", "24h Trading Volume", { role: "style" }];
+          const dataArray = dataFromFireBase.data.reduce((acc, curr) => {
+            curr.exchange.forEach(exch => {
+              if (exch.name === exchangeName) {
+                acc.push([curr.time, parseFloat(exch.volume), getRandomColor()]);
+              }
+            });
+            return acc;
+          }, []);
+          dataArray.unshift(header); // Add header at the beginning of the array
+          return dataArray;
+        };
+        const createSpreadDataArray = (exchangeName) => {
+          const header = ["CEX", "Spread", { role: "style" }];
+          const dataArray = dataFromFireBase.data.reduce((acc, curr) => {
+            curr.exchange.forEach(exch => {
+              if (exch.name === exchangeName) {
+                acc.push([curr.time, exch.spread, getRandomColor()]);
+              }
+            });
+            return acc;
+          }, []);
+          dataArray.unshift(header); // Add header at the beginning of the array
+          return dataArray;
+        };
+
+        const createDepthDataArray = (exchangeName) => {
+          const depthPercentages = ["0.3%", "0.5%", "1%"];
+          const header = [""];
+          const depthData = {
+            "0.3%": [0.3],
+            "0.5%": [0.5],
+            "1%": [1]
+          };
+        
+          for (let i = 0; i < dataFromFireBase.data.length; i++) {
+            const curr = dataFromFireBase.data[i];
+            header.push(curr.time);
+            for (let j = 0; j < curr.exchange.length; j++) {
+              const exch = curr.exchange[j];
+              if (exch.name === exchangeName) {
+                for (let k = 0; k < depthPercentages.length; k++) {
+                  const percentage = depthPercentages[k];
+                  depthData[percentage].push(exch.depth[percentage] || 0);
+                }
+              }
+            }
+          }
+        
+          const result = [header];
+          for (let i = 0; i < depthPercentages.length; i++) {
+            const percentage = depthPercentages[i];
+            result.push(depthData[percentage]);
+          }
+        
+          return result;
+        };
+
+        const Kucoin_data_volume = createVolumeDataArray("Kucoin");
+        const Mexc_data_volume = createVolumeDataArray("Mexc");
+        const Ascendex_data_volume = createVolumeDataArray("Ascendex");
+        const Gate_data_volume = createVolumeDataArray("Gate");
+
+        const Kucoin_data_spread = createSpreadDataArray("Kucoin");
+        const Mexc_data_spread = createSpreadDataArray("Mexc");
+        const Ascendex_data_spread = createSpreadDataArray("Ascendex");
+        const Gate_data_spread = createSpreadDataArray("Gate");
+
+        const Kucoin_data_depth = createDepthDataArray("Kucoin");
+        const Mexc_data_depth = createDepthDataArray("Mexc");
+        const Ascendex_data_depth = createDepthDataArray("Ascendex");
+        const Gate_data_depth = createDepthDataArray("Gate");
+
+        setTV_DATA_KUCOIN(Kucoin_data_volume)
+        setTV_DATA_MEXC(Mexc_data_volume)
+        setTV_DATA_ASD(Ascendex_data_volume)
+        setTV_DATA_GATE(Gate_data_volume)
+
+        setSPREAD_DATA_KUCOIN(Kucoin_data_spread)
+        setSPREAD_DATA_MEXC(Mexc_data_spread)
+        setSPREAD_DATA_ASD(Ascendex_data_spread)
+        setSPREAD_DATA_GATE(Gate_data_spread)
+
+        setDEPTH_DATA_KUCOIN(Kucoin_data_depth)
+        setDEPTH_DATA_MEXC(Mexc_data_depth)
+        setDEPTH_DATA_ASD(Ascendex_data_depth)
+        setDEPTH_DATA_GATE(Gate_data_depth)
+
        
-    },[])
+        
+        console.log('Kucoin_data_depth:', Kucoin_data_depth);
+        console.log('Mexc_data_depth:', Mexc_data_depth);
+        console.log('Ascendex_data_depth:', Ascendex_data_depth);
+        console.log('Gate_data_depth:', Gate_data_depth);
+
+
+      };
+      
+    
+      fetchData();
+    }, []);
+    
 
     useEffect(()=>{
 
@@ -150,73 +325,124 @@ function CH({token}) {
   return (
     <div>
         <center>
-        <input placeholder="Search by Date" style={{height:'2.5em',width:'20em'}} value={searchTerm} onChange={(e)=>{
-            setSearchTerm(e.target.value)
-        }} ></input>
+        <Dropdown>
+      <Dropdown.Toggle style={{backgroundColor: 'white', color:'black'}} id="dropdown-basic">
+            {parameter}
+      </Dropdown.Toggle>
 
+      <Dropdown.Menu>
+
+        <Dropdown.Item  onClick={()=>{
+            
+            setParameter('VOLUME')
            
-<Button variant="outline-primary" onClick={
-    handleSearch
-}>Search</Button>
+          }}>VOLUME</Dropdown.Item>
+
+
+        <Dropdown.Item  onClick={()=>{
+          
+            setParameter('SPREAD')
+           
+          }}>SPREAD</Dropdown.Item>
+
+<Dropdown.Item  onClick={()=>{
+            
+            setParameter('DEPTH')
+           
+          }}>DEPTH</Dropdown.Item>
+
+      </Dropdown.Menu>
+    </Dropdown>
+           
+
 
 <br></br><br></br><br></br>
 
-    {data.map((x)=>{
+{parameter=="VOLUME" && <div>
 
-        let volChart=[
-            ["CEX", "24h Trading Volume", { role: "style" }],
-            ["Kucoin",x.volume[0], "#b87333"], // RGB value
-            ["MEXC", x.volume[1], "silver"], // English color name,
-            ["GATE", x.volume[2], "color: #e5e4e2"], // CSS-style declaration
-            ["ASCENDEX", x.volume[3], "green"],
-        ]
-
-        let spreadChart=[
-            ["CEX", "24h Trading Volume", { role: "style" }],
-            ["Kucoin",x.spread[0], "#b87333"], // RGB value
-            ["MEXC", x.spread[1], "silver"], // English color name,
-            ["GATE", x.spread[2], "color: #e5e4e2"], // CSS-style declaration
-            ["ASCENDEX", x.spread[3], "green"],
-        ]
-
-        let depthChart=[
-        [
-          "Depth",
-          "Kucoin",
-          "MEXC",
-          "GATE",
-          "ASENDEX"
-        ],
-        [0.3,x.depth[0]["0.3%"],x.depth[1]["0.3%"],x.depth[2]["0.3%"],x.depth[3]["0.3%"]],
-        [0.5, x.depth[0]["0.5%"],x.depth[1]["0.5%"],x.depth[2]["0.5%"],x.depth[3]["0.5%"]],
-        [1, x.depth[0]["1%"],x.depth[1]["1%"],x.depth[2]["1%"],x.depth[3]["1%"]],
-        
-      ]
-
-        return(
-            <div>
-  
-
-               <h2> {x.time}</h2>
-               <br></br>
-               <h5>Volume</h5>
-                <Chart chartType="ColumnChart" width="100%" height="400px" data={volChart} options={options} />
-                <br></br>
-               <h5>Spread</h5>
-                <Chart chartType="ColumnChart" width="100%" height="400px" data={spreadChart} options={options} />
-                <br></br>
-               <h5>Depth</h5>
-                <Chart  chartType="Line"
-                  width="100%"
-                  height="400px"
-                  data={depthChart}
-                  options={options} />
-                  <br></br>
+<Chart chartType="ColumnChart" width="100%" height="400px" data={TV_DATA_KUCOIN} options={options} />
+<b>KUCOIN</b>
                 <hr></hr>
+
+               
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={TV_DATA_MEXC} options={options} />
+<b>MEXC</b>
+                
+                <hr></hr>
+               
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={TV_DATA_ASD} options={options} />
+<b>ASCENDEX</b>
+                <hr></hr>
+              
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={TV_DATA_GATE} options={options} />
+<b>GATE</b>
                 <br></br>
-            </div>
-        )
-    })}
+
+</div>}
+
+{parameter=="SPREAD" && <div>
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={SPREAD_DATA_KUCOIN} options={options} />
+
+                <b>KUCOIN</b>
+                <hr></hr>
+
+               
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={SPREAD_DATA_MEXC} options={options} />
+                <b>MEXC</b>
+                <hr></hr>
+                
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={SPREAD_DATA_ASD} options={options} />
+                <b>ASCENDEX</b>
+                <hr></hr>
+              
+
+<Chart chartType="ColumnChart" width="100%" height="400px" data={SPREAD_DATA_GATE} options={options} />
+                <b>GATE</b>
+                <br></br></div>}
+
+
+
+{parameter=="DEPTH" && <div>  <br></br>
+               
+
+<Chart chartType="Line" width="100%" height="400px" data={DEPTH_DATA_KUCOIN} options={options} />
+<br></br><br></br><br></br>
+                <b>KUCOIN</b>
+                <hr></hr>
+
+
+
+
+<Chart chartType="Line" width="100%" height="400px" data={DEPTH_DATA_MEXC} options={options} />
+<br></br><br></br><br></br>
+                <b>MEXC</b>
+                <hr></hr>
+
+
+<Chart chartType="Line" width="100%" height="400px" data={DEPTH_DATA_ASD} options={options} />
+<br></br><br></br><br></br>
+                <b>ASCENDEX</b>
+                <hr></hr>
+
+
+
+
+<Chart chartType="Line" width="100%" height="400px" data={DEPTH_DATA_GATE} options={options} />
+<br></br><br></br><br></br>
+                <b>GATE</b>
+                <hr></hr>
+
+<br></br></div>}
+                <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>
+               
+
+
     </center>
 
     </div>
